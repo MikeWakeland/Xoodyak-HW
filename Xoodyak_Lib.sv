49c335cb25bba041f1e2098bf94654dfbcade74a
MikeWakeland <michael.wakeland@nps.edu>
2021-05-13 03:16:01 -0700

    Code works
    In a very liberal sense of the word.

--- Xoodyak_Lib.sv
+++ Xoodyak_Lib.sv
@@ -1,30 +1,70 @@
 
-/* 
+
     module xoodyaktop(
-      input logic [191:0] plaintext,
+			input logic eph1,
+			input logic reset,
+			input logic start,
+			
+      input logic [191:0] textin,  //Either plain text or cipher text depending on opmode
       input logic [127:0] nonce,
+			input logic [127:0] assodata,
       input logic [127:0] key,
+			input logic	        opmode, //0 for encryption, 1 for decryption.  
 
-      output logic [127:0] authdata,
-      output logic [191:0]   ciphertext,
-      output logic          encdone
+      output logic [127:0]   authdata,
+      output logic [191:0]   textout,
+			output logic					 encdone  //enc and dec appear to be the same thing here.  
 
     );
 
-    logic [383:0] state_initial;
+    logic [383:0] state_initial, state_nonce, state_asso_in, state_asso ;
+		logic		nonce_done, auth_start;
 
-    assign state_initial = {key,8'h01, 256'h0, 8'h2};
+    assign state_initial = {key,16'h01, 232'h0, 8'h2}; //Changed from: {key,8'h01, 240'h0, 8'h2}
+		
+		
     absorb absorbnonce(
-    //instance whatever
+
+	  	.eph1   (eph1),
+			.reset  (reset),
+			.start  (start),
+		
+      .state_in (state_initial),
+      .extra_data (nonce),								 //can be either associated data or the nonce. 
+
+      .state_out (state_nonce),
+      .absorb_done (nonce_done)
+
     );
+  
+	rregs #(384) absnonc ( state_asso_in, state_nonce, eph1);
 
     absorb absorbauthdata(
-    //instance whatever
-    //should be wired directly to nonce.  
-    );
+	  	.eph1   (eph1),
+			.reset  (reset),
+			.start  (nonce_done),
+		
+      .state_in (state_asso_in),
+      .extra_data (assodata),								 //can be either associated data or the nonce. 
 
-    encyrpt encrypt(
-    //does the encryption
+      .state_out (state_asso),
+      .absorb_done (asso_done)
+
+    );
+		logic [383:0] state_enc_in;
+	rregs #(384) endabs ( state_enc_in, state_asso, eph1);
+
+    encrypt encrypt(
+			.eph1 (eph1),
+			.reset (reset),
+			.start (asso_done),
+			
+      .state (state_enc_in),
+      .plaintext (textin),
+      
+      .ciphertext (textout), //from the encrypt modules  (output pin to top)
+      .authdata   (authdata),  //from the encrypt modules (output pin to top)
+			.encdone    (encdone)
     ); 
 
     endmodule: xoodyaktop
@@ -34,12 +74,17 @@
 
 
     module encrypt(
-
+		
+			input logic					eph1,
+			input logic					reset,
+			input	logic					start,
+			
       input logic [383:0] state,
       input logic [191:0] plaintext,
       
       output logic [191:0] ciphertext,
-      output logic [127:0] authdata
+      output logic [127:0] authdata,
+			output logic         encdone
 
     );
 
@@ -49,32 +94,43 @@
 
     assign state_enc = {state[383:8],~state[7],state[6:0]}; //XORS domain value 0x80 with the state upon start of encrytion. 
 
-    round etcwhatever (
-
-    .state_in (state_enc),
-    .state_out (enc_permd),
-    .xood_done (encpermflag)
+    permute encperm (  //May only need one set of permute hardware.  It may operate  asynchronously from the clock.  
+		.eph1						(eph1),
+		.reset					(reset),
+		.start 					(start),
+    .state_in       (state_enc),
+    .state_out      (enc_permd),
+    .xood_done      (encpermflag)
     );
 
     //XORS up to 192' of message with the state.  
     //assumption: xor'ing with the 192 MSB of the state.
 
     assign ciphertext = plaintext ^ enc_permd[383:192];
-
-    logic [127:0] final_authtag
+		
     
-    squeeze squeezetag(
-      .state   (enc_permd),
-      .authtag ( final_authtag)
+    logic [127:0] final_authtag;
+    
+		logic [383:0] sqz_in, sqz_ciphertext; 
+		assign sqz_ciphertext = {ciphertext, enc_permd[191:185], ~enc_permd[184], enc_permd[183:0]};  //a hex 0x01 XOR at the most significant bit after the ciphertext.  
+		rregs #(384) encsqz ( sqz_in ,sqz_ciphertext, eph1);
+		
+    squeeze squeezetag(  //consider making this in the top module.  pass to enc and dec as needed based on the situation.  
+       .eph1 (eph1),
+			.reset (reset),
+			.start (encpermflag),
+			
+			.state   (sqz_in),
+      .authtag ( final_authtag),
+			.sq_done (encdone)//whenever we're done with both generating the ciphertext and the authdata
+			
     );
 
-    assign ciphertext = enc_vec;
     assign authdata   = final_authtag;
-    assign encdone = ;//whenever we're done with both generating the ciphertext and the authdata
-
+		
     endmodule: encrypt 
 
- */
+ 
 
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -84,7 +140,7 @@
 
 			input logic 				eph1,
 			input logic					reset, 
-			input logic 				start,
+			input logic 				start,  //start has to be a pulse.  
       input logic  [383:0] state_in,  //Indicies: plane, lane, zed
       
       output logic [383:0] state_out,
@@ -98,16 +154,19 @@
 
     logic [3:0] rnd_cnt, rnd_cnt_next;
     //fake counter, will be replaced by direct evaluation.  
-    initial rnd_cnt = 12;
+      
+		
     assign rnd_cnt_next = rnd_cnt - 1;  
-    rregs #(4) xooctr (rnd_cnt, rnd_cnt_next,eph1);
-    assign xood_done = (rnd_cnt == 4'h0);
+		
+    rregs #(4) xooctr (rnd_cnt, start? 4'hc : rnd_cnt_next ,eph1);
+		//assign rnd_cnt = start ? 4'hc : rnd_cnt_next; //Get the feeling I'm going to be off by one round here.
+    assign xood_done = (rnd_cnt == 4'h1);
+		
 		logic [2:0][3:0][31:0] rnd_recycle; 
 		logic [383:0] bits_in, bits_le;
     logic [2:0][3:0][31:0] perm_in ; //Indicies: plane, lane, zed
     //Traditioanl formatting: assign perm_in = state_in;
-		
-		assign bits_in = (rnd_cnt == 11) ? state_in : rnd_recycle; 
+				assign bits_in = (rnd_cnt == 12) ? state_in : rnd_recycle; 
 		assign bits_le = {bits_in[359:352], bits_in[367:360], bits_in[375:368], bits_in[383:376],
 											bits_in[327:320],bits_in[335:328],bits_in[343:336],bits_in[351:344],
 											bits_in[295:288],bits_in[303:296],bits_in[311:304],bits_in[319:312],
@@ -127,7 +186,7 @@
     logic [3:0][31:0] p, e; //Indicies: lane, zed.
 		logic [2:0][3:0][31:0] rnd_input;
 
-		assign rnd_input = (rnd_cnt == 11) ? bits_le : rnd_recycle;
+		assign rnd_input = (rnd_cnt == 12) ? bits_le : rnd_recycle;
     assign p =  rnd_input[0]^rnd_input[1]^rnd_input[2];  //Will need to make a better version later.  
 
     //write as function perhaps191
@@ -182,11 +241,11 @@
 
 			logic [31:0] const_wire;
 			
-			assign const_wire = CIBOX[rnd_cnt+1];
+			assign const_wire = CIBOX[rnd_cnt];
 			
 			//The python script definitely doesn't lookup anything except the first set
 			//Per the tech standard, it should be modifiying rho_west[0][0] instead but endianness is getting in the way.  
-      assign rho_west[2][3] = theta_out[2][3]^ CIBOX[rnd_cnt+1];
+      assign rho_west[2][3] = theta_out[2][3]^ CIBOX[rnd_cnt];
       assign rho_west[2][2] = theta_out[2][2]; 
       assign rho_west[2][1] = theta_out[2][1]; 
       assign rho_west[2][0] = theta_out[2][0] ; //^ CIBOX[rnd_cnt]; 
@@ -243,13 +302,29 @@
    
 	 assign rho_east[2] = chi_out[2];
 
-    assign state_out = rho_east;
+		logic [383:0] round_out;
+    
+		assign round_out = rho_east;
 
 		
-		rregs  #(384) rndwire (rnd_recycle ,state_out,eph1);
+		rregs  #(384) rndwire (rnd_recycle ,round_out,eph1);
 		logic [11:0][31:0] state_wiretap;
-		assign state_wiretap = state_out;
+		assign state_wiretap = round_out;
 
+    //Rotate the state to get it to comply with the C code again for the output:  
+		
+		assign state_out = {round_out[359:352], round_out[367:360], round_out[375:368], round_out[383:376],
+									round_out[327:320],round_out[335:328],round_out[343:336],round_out[351:344],
+									round_out[295:288],round_out[303:296],round_out[311:304],round_out[319:312],
+									round_out[263:256],round_out[271:264],round_out[279:272],round_out[287:280],
+									round_out[231:224],round_out[239:232],round_out[247:240],round_out[255:248],
+									round_out[199:192],round_out[207:200],round_out[215:208],round_out[223:216],
+									round_out[167:160],round_out[175:168],round_out[183:176],round_out[191:184],
+									round_out[135:128],round_out[143:136],round_out[151:144],round_out[159:152],
+									round_out[103:96] ,round_out[111:104],round_out[119:112],round_out[127:120],
+									round_out[71:64]  ,round_out[79:72]  ,round_out[87:80]  ,round_out[95:88],
+									round_out[39:32]  ,round_out[47:40]  ,round_out[55:48]  ,round_out[63:56],
+									round_out[7:0]    ,round_out[15:8]   ,round_out[23:16]  ,round_out[31:24]};
 
 
 
@@ -287,15 +362,25 @@
     );
 
     //Critically, I don't know what order I'm XORing, so the array indices may be backwards here.  Assuming I'm starting at C index 0 = msb
-    assign state_temp[135:8] = extra_data^perm_out[383:256]; //Absorbs the nonce or AD from bytes 0-15 inclusive
-    assign state_temp[7:0] = {perm_out[255:249], ~perm_out[248]}; //XOR's 0x01 with byte 16
+		//Swapped due to C code from [135:8] [7:0] to [135:128] [127:0]
+		//Also adding in inverters at bit position 0, 1, 247.  Why? because it matches the software.  
+		
+		
+		assign state_temp[135:128] = {perm_out[255:249], ~perm_out[248]}; //XOR's 0x01 with byte 16
+    assign state_temp[127:0] = extra_data^perm_out[383:256]; //Absorbs the nonce or AD from bytes 0-15 inclusive
+		
+		
+/* 		assign state_temp[7:0] = {perm_out[255:249], ~perm_out[248]}; //XOR's 0x01 with byte 16
+    assign state_temp[135:8] = extra_data^perm_out[383:256]; //Absorbs the nonce or AD from bytes 0-15 inclusive */
+
 
 
     //Set phase_down, if applicable (per the standard but may not be useful in HW)
 
-    assign state_out = {state_temp, perm_out[255:0]};
+    //assign state_out = {state_temp, perm_out[255:0]};
+    assign state_out = {state_temp, perm_out[255:249], ~perm_out[248] ,perm_out[247:2], ~perm_out[1:0]};
 
-    //assign absorb_done = something, idk?;
+    assign absorb_done = perm_done;
 
 
     endmodule: absorb
@@ -303,12 +388,13 @@
 
     module squeeze (
 		
-	 	 input logic 				eph1,
+	 	 input logic 				  eph1,
 			input logic					reset, 
 			input logic 				start,
 
       input logic [383:0] state,
-      output logic [127:0] authtag
+      output logic [127:0] authtag,
+			output logic sq_done
 
     );
     logic [383:0] perm_in, perm_out;
@@ -321,13 +407,13 @@
 		.eph1			 (eph1),
 		.reset		 (reset),
 		.start		 (start),
-    .state_in  (state),
+    .state_in  (perm_in),
     .state_out (perm_out),
     .xood_done (perm_done)
 		
     );
 
     assign authtag = perm_out[127:0]; //is it 127:0 or 383:255 191
-
+   assign sq_done = perm_done;
     endmodule: squeeze
  
--- Xoodyak_testbench.sv
+++ Xoodyak_testbench.sv
@@ -38,9 +38,26 @@
        logic  [383:0] state_in;  //Indicies: plane, lane, zed
       
        logic [383:0] state_out;
-       logic xood_done; 	
+       logic xood_done, opmode; 	
 
+ logic start_p;
+  rregs #(1)  strt (start_p , ~reset , eph1); 
+	assign start = ~start_p & ~reset; 
 	assign state_in = 384'h000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f;
+	
+	logic [127:0]  nonce_t, asso_data_t, key_t;
+	logic [191:0] plaintext_t;
+	assign plaintext_t = 192'h4142434445464748494a4b4c4d4e4f505152535455565758;  //ascii text: ABCDEFGHIJKLMNOP QRST UVWX
+	
+	assign nonce_t = 128'h303132333435363738393a3b3c3d3e3f;  //ascii text:  0123456789:;<=>?
+	assign asso_data_t= 128'h4142434445464748494a4b4c4d4e4f50;  //ascii text: ABCDEFGHIJKLMNOP
+	assign key_t = 128'h6162636465666768696a6b6c6d6e6f70; //ascii text: abcdefghijklmnop
+	assign opmode = 1'b0;
+	
+	
+	
+	logic [127:0] authdata_o, textout_o;
+	logic encdone;
 		
 				
 /////////////////////////////////////////////////////End fake input section///////////////////////////////////////////////////////		
@@ -48,7 +65,7 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-
+/* 
 	 xooround execute (
 	 
 				.eph1				(eph1),
@@ -60,6 +77,28 @@
 				
 				.xood_done 	(xood_done)
 		 );
+		  */
+		 
+	xoodyaktop testbench(
+			 .eph1 (eph1),
+			 .reset (reset),
+			 .start (start),
+			
+       .textin    (plaintext_t),                                        //Either plain text or cipher text depending on opmode
+       .nonce   (nonce_t),
+			 .assodata (asso_data_t),
+       .key      (key_t),
+			.opmode    (opmode),                                   //0 for encryption, 1 for decryption.  
+
+      .authdata (authdata_o),
+      .textout  (textout_o),
+			.encdone  (encdone) //enc and dec appear to be the same thing here.  
+
+    );
+
+		 
+		 
+		 
 		
 		endmodule: tb_top
  
